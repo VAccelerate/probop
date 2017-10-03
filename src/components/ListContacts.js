600e03028d8462cfc3ba41808e61bd2068eac231
Pauline <pauline.design007@gmail.com>
2017-10-04 09:57:10 +1300

    created link in app.js

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-export default class Example extends React.Component {
+export default class ListContacts extends React.Component {
   render() {
     return (
       <Form>
