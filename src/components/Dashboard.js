eab5a523eab763e6bf0de1eed900dd2dbb2fe32e
amandahogan <hogan.ans@gmail.com>
2017-10-04 09:46:42 +1300

    Move Modal into SafeModal component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import { 
-        Button,
-        Modal,
-        ModalBody,
-        ModalFooter
-} from 'reactstrap'
-// import SafeModal from './SafeModal'
+import { Button } from 'reactstrap'
+
+import SafeModal from './SafeModal'
 
 class Dashboard extends Component{
-
-  constructor(props) {
+  constructor(props){
     super(props)
     this.state = {modal: false}
     this.toggle = this.toggle.bind(this)
@@ -35,15 +30,7 @@ class Dashboard extends Component{
         <Button color="warning" onClick={this.toggle}>
           <h3>I&#39;m feeling vulnerable</h3>
         </Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} autoFocus={false}>
-          <ModalBody>
-            <h1>Danger alert sent!</h1>
-            <p>TBC Help is on the way</p>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="success" onClick={this.toggle}>I&#39;m feeling safe now</Button>
-          </ModalFooter>
-        </Modal>
+        <SafeModal toggle={this.toggle} modal={this.state.modal} />
       </div>
     )
   }
--- src/components/SafeModal.js
+++ src/components/SafeModal.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
-import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
+import React from 'react'
+import { 
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap'
 
 const SafeModal = (props) => {
   return(
-    <Modal isOpen={props.modal} toggle={props.toggle}>
+    <Modal isOpen={props.modal} toggle={props.toggle} autoFocus={false}>
       <ModalBody>
-        Danger Alert Sent!
+        <h1>Danger alert sent!</h1>
+        <p>TBC Help is on the way</p>
       </ModalBody>
       <ModalFooter>
-        <Button color="primary" onClick={props.toggle}>
-          Im feeling safe now
+        <Button color="success" onClick={props.toggle}>
+          I&#39;m feeling safe now
         </Button>
       </ModalFooter>
     </Modal>
