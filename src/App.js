861f72af76eec7242eecab162631f0876c01010e
Pauline <pauline.design007@gmail.com>
2017-10-04 12:44:01 +1300

    added management of Contacts

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard'
 import Contacts from './components/Contacts'
-import ListContacts from './components/ListContacts'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
         <Contacts />
-        <ListContacts />
+
       </div>
+
     );
   }
 }
 
-export default App;
+export default App
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading, Button } from 'reactstrap'
+import ManageContacts from './ManageContacts'
 
 //ListGroup AND ListGroupItem does not exist but are required.
 
-export default class Contacts extends React.Component {
+class Contacts extends React.Component {
+
+  constructor(props){
+      super(props)
+      this.state = {manageVisible: false}
+      this.toggle = this.toggle.bind(this)
+    }
+
+    toggle(){
+      this.setState({
+        manageVisible: !this.state.manageVisible
+        })
+    }
+
   render() {
     return (
-      <ListGroup>
-        <ListGroupItem disabled tag="a" href="#">Jeremy</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Pauline</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Zayne</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Damon</ListGroupItem>
-        <ListGroupItem tag="a" href="#">BopSass</ListGroupItem>
-      </ListGroup>
-    );
+      <div>
+        {!this.state.manageVisible ?
+          <div>
+            <h1>Contacts</h1>
+            <ListGroup>
+              <ListGroupItem>
+                <ListGroupItemHeading>Pauline Stokes</ListGroupItemHeading>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ListGroupItemHeading>Jeremy Liss</ListGroupItemHeading>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ListGroupItemHeading>Amanda Hogan</ListGroupItemHeading>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ListGroupItemHeading>Zayne Imtiaz</ListGroupItemHeading>
+              </ListGroupItem>
+            </ListGroup><br></br>
+            <Button color="primary" size="lg" onClick={this.toggle}>
+              Add Contact
+            </Button>
+          </div>
+          :
+          <ManageContacts />
+        }
+      </div>
+
+    )
   }
 }
+
+export default Contacts
