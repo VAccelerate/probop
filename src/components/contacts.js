8d52e411ab186e0f3b8b9d45981ce6ed76563582
Pauline <pauline.design007@gmail.com>
2017-10-03 17:05:17 +1300

    added contacts component

---
+++ src/components/contacts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+
+const Contacts = (props) => {
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <Button color="danger">danger</Button>
+        <Button color="warning">warning</Button>
+      </div>
+    )
+}
+
+export default Contacts
