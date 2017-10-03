25b133697d4dc1e19be1667b4a14aa56e902140a
imtiazy <zane.imtiaz@vodafone.com>
2017-10-03 16:49:47 +1300

    Added urgent and vulnerable buttons to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,8 +5,15 @@ const Dashboard = (props) => {
     return (
       <div>
         <h1>Dashboard</h1>
-        <Button color="danger">danger</Button>
-        <Button color="warning">warning</Button>
+        <Button color="danger">
+          <h3>I need urgent help</h3>
+          <p>TBC</p>
+        </Button>
+        <br/>
+        <br/>
+        <Button color="warning">
+          <h3>I&#39;m feeling vulnerable</h3>
+          </Button>
       </div>
     )
 }
