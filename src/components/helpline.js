8cdd731286106f7174cf0406eae5bcf626adf227
Pauline <pauline.design007@gmail.com>
2017-10-03 16:45:55 +1300

    added helpline component

---
+++ src/components/helpline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+
+const Helpline = (props) => {
+    return (
+      <div>
+        <h1>Helpline</h1>
+    
+      </div>
+    )
+}
+
+export default Helpline
