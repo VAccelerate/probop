91b46bd23c0f05773d324f856fe03047e079f4e7
Pauline <pauline.design007@gmail.com>
2017-10-05 11:52:55 +1300

    added content to helpline.js

--- src/components/Helplines.js
+++ src/components/Helplines.js
@@ -9,14 +9,50 @@ const Helplines = (props) => {
       <h1>Helplines</h1>
       <table>
         <tr>
-          <th>Helpline</th>
-          <th>Contact</th>
-          <th>URL</th>
-        </tr>
-        <tr>
-          <td>Alcohol & Drug Helpline</td>
+          <th>Alcohol & Drug Helpline</th>
           <td>0800 787 797</td>
           <td>alcoholdrughelp.org.nz</td>
+        </tr>
+        <tr>
+          <th>Are you Okay (violence helpline)</th>
+          <td>0800 456 450</td>
+          <td>www.areyouok.org.nz</td>
+        </tr>
+        <tr>
+          <th>Depression Hotline</th>
+          <td>0800 111 757</td>
+          <td>www.depression.org.nz</td>
+        </tr>
+        <tr>
+          <th>Lifeline (crisis support)</th>
+          <td>0800 543 354</td>
+          <td>www.lifeline.org.nz</td>
+        </tr>
+        <tr>
+          <th>Pacifika helpline</th>
+          <td>0800 787 799</td>
+          <td>TXT: 8681</td>
+        </tr>
+        <tr>
+          <th>Outline (Sexuality & Gender support)</th>
+          <td>0800 688 5463</td>
+          <td>www.outline.org.nz</td>
+        </tr>
+        <tr>
+          <th>Shakti (Ethnic women support)</th>
+          <td>0800 742 584</td>
+          <td>shakti-international.org</td>
+        </tr>
+        <tr>
+          <th>Youthline</th>
+          <td>0800 376 633</td>
+          <td>TXT 234</td>
+          <td>www.youthline.co.nz</td>
+        </tr>
+        <tr>
+          <th>Safe to Talk (Sexaul assault support line)</th>
+          <td>TBC</td>
+          <td>TBC</td>
         </tr>
       </table>
     </div>
