5cb974603360be01b13ef6babf401fb462d8bb09
amandahogan <hogan.ans@gmail.com>
2017-10-04 14:17:35 +1300

    Change HashRouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Dashboard from './components/Dashboard'
 import Profile from './components/Profile'
 import Helplines from './components/Helplines'
 
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/helplines' component={Helplines} />
-      </div>
-    </Router>
-  )
-}
-  
+const App = (props) => (
+  <div>
+    <Switch>
+      <Route exact path='/' component={Dashboard}/>
+      <Route path='/profile' component={Profile}/>
+      <Route path='/helplines' component={Helplines}/>
+    </Switch>
+  </div>
+)
+
 export default App
