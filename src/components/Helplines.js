edba976c74ea51c494dc00415e36b54bfce7cf83
Pauline <pauline.design007@gmail.com>
2017-10-05 12:38:10 +1300

    edited content information using Group Items

--- src/components/Helplines.js
+++ src/components/Helplines.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { ListGroup, ListGroupItem, ListGroupItemHeading, Button } from 'reactstrap'
 import Menu from './Menu'
 
 const Helplines = (props) => {
@@ -7,54 +7,63 @@ const Helplines = (props) => {
     <div>
       <Menu />
       <h1>Helplines</h1>
-      <table>
-        <tr>
-          <th>Alcohol & Drug Helpline</th>
-          <td>0800 787 797</td>
-          <td>alcoholdrughelp.org.nz</td>
-        </tr>
-        <tr>
-          <th>Are you Okay (violence helpline)</th>
-          <td>0800 456 450</td>
-          <td>www.areyouok.org.nz</td>
-        </tr>
-        <tr>
-          <th>Depression Hotline</th>
-          <td>0800 111 757</td>
-          <td>www.depression.org.nz</td>
-        </tr>
-        <tr>
-          <th>Lifeline (crisis support)</th>
-          <td>0800 543 354</td>
-          <td>www.lifeline.org.nz</td>
-        </tr>
-        <tr>
-          <th>Pacifika helpline</th>
-          <td>0800 787 799</td>
-          <td>TXT: 8681</td>
-        </tr>
-        <tr>
-          <th>Outline (Sexuality & Gender support)</th>
-          <td>0800 688 5463</td>
-          <td>www.outline.org.nz</td>
-        </tr>
-        <tr>
-          <th>Shakti (Ethnic women support)</th>
-          <td>0800 742 584</td>
-          <td>shakti-international.org</td>
-        </tr>
-        <tr>
-          <th>Youthline</th>
-          <td>0800 376 633</td>
-          <td>TXT 234</td>
-          <td>www.youthline.co.nz</td>
-        </tr>
-        <tr>
-          <th>Safe to Talk (Sexaul assault support line)</th>
-          <td>TBC</td>
-          <td>TBC</td>
-        </tr>
-      </table>
+      <ListGroup>
+        <ListGroup>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              Alcohol & Drug Helpline
+            </ListGroupItemHeading>
+            <p>0800 787 797<br></br>https://alcoholdrughelp.org.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              Are you Okay (Violence Helpline)
+            </ListGroupItemHeading>
+            <p>0800 456 450<br></br>http://www.areyouok.org.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              Depression Hotline
+            </ListGroupItemHeading>
+            <p>0800 111 757<br></br>TXT: 4202 <br></br> https://depression.org.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              Lifeline (Crisis Support)
+            </ListGroupItemHeading>
+            <p>0800 543 354<br></br>https://www.lifeline.org.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              Pacifika Helpline
+            </ListGroupItemHeading>
+            <p>0800 787 799<br></br>TXT: 8681</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+          Outline (Sexuality & Gender Support)
+            </ListGroupItemHeading>
+            <p>0800 688 5463<br></br>http://www.outline.org.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+          Shakti (Ethnic women support)
+            </ListGroupItemHeading>
+            <p>0800 742 584<br></br>http://shakti-international.org/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+          Youthline
+            </ListGroupItemHeading>
+            <p>0800 376 633<br></br>TXT 234<br></br>https://www.youthline.co.nz/</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+          Safe to Talk (Sexaul Assault Support Line)
+            </ListGroupItemHeading>
+          </ListGroupItem>
+        </ListGroup>
+      </ListGroup>
     </div>
   )
 }
