8dfc2b9082b270129fcacfe69982648f91c80772
Jeremy Liss <jeremyliss@gmail.com>
2017-10-04 17:12:35 +1300

    add react carousel and button

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
+import { Button } from 'reactstrap'
 import { Carousel } from 'react-responsive-carousel'
+import { Link } from 'react-router-dom'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 class Guide extends Component {
-    render() {
-        return (
-            <Carousel autoPlay>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
-                    <p className="legend">Legend 4</p>
-                </div>
-            </Carousel>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Carousel showThumbs={false} dynamicHeight={false} >
+          <div>
+            <img src="http://placekitten.com/300/500" />
+            <p className="legend">WELCOME!</p>
+          </div>
+          <div>
+            <img src="http://placekitten.com/350/600" />
+            <p className="legend">How to use the BOPSASS app</p>
+          </div>
+          <div>
+            <img src="http://placekitten.com/360/600" />
+            <p className="legend">Legend 3</p>
+          </div>
+          <div>
+            <img src="http://placekitten.com/350/600" />
+            <p className="legend">Legend 4</p>
+          </div>
+        </Carousel>
+        <Button color="warning" >
+          <Link to="/profile">
+            <h3>Get Started</h3>
+          </Link>
+        </Button>
+      </div>
+   )
+  }
 }
 
 export default Guide
