101a4058b3384517253b38856ae92f2758a71edd
Pauline <pauline.design007@gmail.com>
2017-10-05 14:58:40 +1300

    added phones icons

--- src/components/Helplines.js
+++ src/components/Helplines.js
@@ -21,43 +21,43 @@ const Helplines = (props) => {
             <ListGroupItemHeading>
               Are you Okay (Violence Helpline)
             </ListGroupItemHeading>
-            <p>0800 456 450<br />http://www.areyouok.org.nz/</p>
+            <p><FontAwesome.FaPhone />0800 456 450<br />http://www.areyouok.org.nz/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
               Depression Hotline
             </ListGroupItemHeading>
-            <p>0800 111 757<br />TXT: 4202 <br /> https://depression.org.nz/</p>
+            <p><FontAwesome.FaPhone />0800 111 757<br />TXT: 4202 <br /> https://depression.org.nz/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
               Lifeline (Crisis Support)
             </ListGroupItemHeading>
-            <p>0800 543 354<br />https://www.lifeline.org.nz/</p>
+            <p><FontAwesome.FaPhone />0800 543 354<br />https://www.lifeline.org.nz/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
               Pacifika Helpline
             </ListGroupItemHeading>
-            <p>0800 787 799<br />TXT: 8681</p>
+            <p><FontAwesome.FaPhone />0800 787 799<br />TXT: 8681</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
           Outline (Sexuality & Gender Support)
             </ListGroupItemHeading>
-            <p>0800 688 5463<br />http://www.outline.org.nz/</p>
+            <p><FontAwesome.FaPhone />0800 688 5463<br />http://www.outline.org.nz/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
           Shakti (Ethnic women support)
             </ListGroupItemHeading>
-            <p>0800 742 584<br />http://shakti-international.org/</p>
+            <p><FontAwesome.FaPhone />0800 742 584<br />http://shakti-international.org/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
           Youthline
             </ListGroupItemHeading>
-            <p>0800 376 633<br />TXT 234<br />https://www.youthline.co.nz/</p>
+            <p><FontAwesome.FaPhone />0800 376 633<br />TXT 234<br />https://www.youthline.co.nz/</p>
           </ListGroupItem>
           <ListGroupItem>
             <ListGroupItemHeading>
