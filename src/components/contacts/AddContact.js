5050ce12d8afc7d5f10fd0507f06c23aa965cf83
imtiazy <zane.imtiaz@vodafone.com>
2017-10-10 09:36:52 +1300

    style fixes

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,8 +1,7 @@
 /* global localStorage */
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, Modal, ModalBody, ModalFooter } from 'reactstrap'
-
-import Menu from '../Menu'
+import { Redirect } from 'react-router'
 import {
   CONTACTS,
   CONTACT_NAME,
@@ -17,11 +16,11 @@ class AddContact extends Component {
     this.state = {
       duplicateModal: false,
       emptyModal: false,
-      successModal: false
+      added: false,
+      name: ''
     }
     this.duplicateModalToggle = this.duplicateModalToggle.bind(this)
     this.emptyModalToggle = this.emptyModalToggle.bind(this)
-    this.successModalToggle = this.successModalToggle.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
   }
 
@@ -63,7 +62,11 @@ class AddContact extends Component {
     }
     // convert object to json string and save
     localStorage.setItem(CONTACTS, JSON.stringify(contacts))
-    this.successModalToggle()
+    // Used for redirect back to manage contacts page
+    this.setState({
+      added: true,
+      name
+    })
   }
 
   duplicateModalToggle () {
@@ -78,44 +81,46 @@ class AddContact extends Component {
     })
   }
 
-  successModalToggle () {
-    this.setState({
-      successModal: !this.state.successModal
-    })
-  }
-
   render () {
+    const {added, name} = this.state
     return (
       <div>
-        <Menu />
-        <h1>Add Contact</h1>
-        <Form onSubmit={this.onSubmit}>
-          <FormGroup>
-            <Label>Name</Label>
-            <Input type='text' id={CONTACT_NAME} />
-          </FormGroup>
-          <FormGroup>
-            <Label>Mobile Number</Label>
-            <Input type='tel' id={CONTACT_NUMBER} />
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type='checkbox' id={CONTACT_VULNERABLE} />{' '}
+        {added
+        ? <Redirect to={{
+          pathname: '/contacts',
+          state: {
+            added: true,
+            name
+          }
+        }} />
+        : <div>
+          <Form onSubmit={this.onSubmit}>
+            <FormGroup>
+              <Label>Name</Label>
+              <Input type='text' id={CONTACT_NAME} />
+            </FormGroup>
+            <FormGroup>
+              <Label>Mobile Number</Label>
+              <Input type='tel' id={CONTACT_NUMBER} />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='checkbox' id={CONTACT_VULNERABLE} />{' '}
             Vulnerable
           </Label>
-            <Label check>
-              <Input type='checkbox' id={CONTACT_DANGER} />{' '}
+              <Label check>
+                <Input type='checkbox' id={CONTACT_DANGER} />{' '}
               Danger
           </Label>
-          </FormGroup>
-          <Button color='primary' size='lg'>Save Changes</Button>{' '}
-        </Form>
-        <ContactsModal toggle={this.duplicateModalToggle} modal={this.state.duplicateModal}
-          message='Contact with the same name already exists' />
-        <ContactsModal toggle={this.emptyModalToggle} modal={this.state.emptyModal}
-          message='Name and number are required' />
-        <ContactsModal toggle={this.successModalToggle} modal={this.state.successModal}
-          message='Contact successfully added' />
+            </FormGroup>
+            <Button color='primary' size='lg'>Save Changes</Button>{' '}
+          </Form>
+          <ContactsModal toggle={this.duplicateModalToggle} modal={this.state.duplicateModal}
+            message='Contact with the same name already exists' />
+          <ContactsModal toggle={this.emptyModalToggle} modal={this.state.emptyModal}
+            message='Name and number are required' />
+        </div>
+        }
       </div>
     )
   }
--- src/components/contacts/ManageContacts.js
+++ src/components/contacts/ManageContacts.js
@@ -1,22 +1,36 @@
 /* global localStorage */
 import React, { Component } from 'react'
-import { ListGroup, ListGroupItem, Button } from 'reactstrap'
+import { ListGroup, ListGroupItem, Button, Alert } from 'reactstrap'
 import { Link } from 'react-router-dom'
-
 import Menu from '../Menu'
+
+// import Menu from '../Menu'
 import { CONTACTS } from '../../constants'
 
 class ManageContacts extends Component {
   constructor (props) {
     super(props)
-    this.state = {manageVisible: false}
+    let contactAdded = false
+    let contactEdited = false
+    let name = ''
+    if (this.props.location.state) {
+      contactAdded = this.props.location.state.added || false
+      contactEdited = this.props.location.state.edited || false
+      name = this.props.location.state.name || ''
+    }
+    this.state = {
+      contactAdded,
+      contactEdited,
+      name
+    }
     this.toggle = this.toggle.bind(this)
     this.getContacts = this.getContacts.bind(this)
   }
 
   toggle () {
     this.setState({
-      manageVisible: !this.state.manageVisible
+      contactAdded: false,
+      contactEdited: false
     })
   }
 
@@ -28,11 +42,20 @@ class ManageContacts extends Component {
   }
 
   render () {
+    const {contactAdded, contactEdited, name} = this.state
     return (
       <div>
+        <Menu />
         <div>
-          <Menu />
-          <h1>Contacts</h1>
+          <br />
+          <br />
+          <br />
+          <Alert color='dark' isOpen={contactAdded} toggle={this.toggle}>
+            Done! {name} was added to your contact list
+          </Alert >
+          <Alert color='dark' isOpen={contactEdited} toggle={this.toggle}>
+            Great! {name} was updated
+          </Alert >
           <ListGroup>
             {
               this.getContacts().map(id =>
