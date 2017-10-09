59a015b0418bf7522ea50f53d6614ad25b382f20
amandahogan <hogan.ans@gmail.com>
2017-10-09 16:43:54 +1300

    TA-DA Buttons are in the middle of page everytime

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 
 import SafeModal from './SafeModal'
-import Menu from './Menu'
 import { getUserLocation } from '../helpers'
 import './Dashboard.css'
 
@@ -64,24 +63,14 @@ class Dashboard extends Component {
       <div>
         {this.state.showUserLocation && <div id='googlemaps'><img className='stretch' src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.userLocation.latitude},${this.state.userLocation.longitude}&zoom=16&size=480x640&markers=color:red|${this.state.userLocation.latitude},${this.state.userLocation.longitude}&key=${GOOGLE_API_KEY}`} alt='' /></div>}
         <div id='alerts' className='buttonContainer'>
-          <Menu />
           <a href={this.state.contact + this.state.dangerMessage}>
-            <Button className='danger' onClick={this.handleClick} block>
-              <h3>I need urgent help</h3>
-              <p>TBC</p>
-            </Button>
+            <Button className='danger' onClick={this.handleClick} block>Help! I'm in danger</Button>
           </a>
-          <br />
-          <br />
           <a href={this.state.contact + this.state.vulnerableMessage}>
-            <Button className='warning' onClick={this.handleClick}>
-              <h3>I&#39;m feeling vulnerable</h3>
-            </Button>
+            <Button className='warning' onClick={this.handleClick} block>I feel unsafe</Button>
           </a>
         </div>
         <SafeModal toggle={this.toggle} modal={this.state.modal} />
-        <br />
-        <br />
         {this.state.showUserLocation && <a href={`http://www.google.com/maps/place/${this.state.userLocation.latitude},${this.state.userLocation.longitude}`}>See Where I am</a>}
       </div>
     )
