feaf122e42cac041531ecf1f2f7bd152bfce6c5e
Jeremy Liss <jeremyliss@gmail.com>
2017-10-10 16:09:37 +1300

    remove console log

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -43,9 +43,6 @@ class Dashboard extends Component {
     this.toggle()
 
     if (level === 'danger') {
-
-      console.log(this.state.dangerContacts + (this.state.dangerMessage + `http://www.google.com/maps/place/${this.state.userLocation.latitude},${this.state.userLocation.longitude}`))
-
       window.location = this.state.dangerContacts + (this.state.dangerMessage + `http://www.google.com/maps/place/${this.state.userLocation.latitude},${this.state.userLocation.longitude}`)
       this.setState({
         modalMessage: 'Danger alert sent!',
