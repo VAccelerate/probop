38cf4bdda16c54a68450983129bc619cb576fa07
Jeremy Liss <jeremyliss@gmail.com>
2017-10-10 15:52:29 +1300

    add style to safeModal

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,6 +14,10 @@ class Dashboard extends Component {
     super(props)
     this.state = {
       modal: false,
+      modalMessage: 'Danger alert sent!',
+      modalStyle: {
+        backgroundColor: '#e60000'
+      },
       vulnerableContacts: '',
       dangerContacts: '',
       vulnerableMessage: '?body=I am feeling unsafe. This is my location: ',
@@ -40,9 +44,21 @@ class Dashboard extends Component {
 
     if (level === 'danger') {
       window.location = this.state.dangerContacts + (this.state.dangerMessage + `http://www.google.com/maps/place/${this.state.userLocation.latitude},${this.state.userLocation.longitude}`)
+      this.setState({
+        modalMessage: 'Danger alert sent!',
+        modalStyle: {
+          backgroundColor: '#e60000'
+        }
+      })
     }
     if (level === 'vulnerable') {
       window.location = this.state.vulnerableContacts + (this.state.vulnerableMessage + `http://www.google.com/maps/place/${this.state.userLocation.latitude},${this.state.userLocation.longitude}`)
+      this.setState({
+        modalMessage: 'Unsafe alert sent!',
+        modalStyle: {
+          backgroundColor: '#14afb8'
+        }
+      })
     }
   }
 
@@ -61,7 +77,6 @@ class Dashboard extends Component {
     })
 
     if (localStorage.getItem(CONTACTS)) {
-
       let dangerNumbers = getContactNumbers('CONTACT_DANGER', JSON.parse(localStorage.getItem(CONTACTS)))
       let vulnerableNumbers = getContactNumbers('CONTACT_VULNERABLE', JSON.parse(localStorage.getItem(CONTACTS)))
 
@@ -90,10 +105,10 @@ class Dashboard extends Component {
       <div>
         {this.state.showUserLocation && <div id='googlemaps'><img className='stretch' src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.userLocation.latitude},${this.state.userLocation.longitude}&zoom=16&size=480x640&markers=color:red|${this.state.userLocation.latitude},${this.state.userLocation.longitude}&key=${GOOGLE_API_KEY}`} alt='' /></div>}
         <div id='alerts' className='buttonContainer'>
-          <Button className='danger' onClick={(e)=>this.handleClick(e, 'danger')} block>Help! I&#39;m in danger</Button>
-          <Button className='warning' onClick={(e)=>this.handleClick(e, 'vulnerable')} block>I feel unsafe</Button>
+          <Button className='danger' onClick={(e) => this.handleClick(e, 'danger')} block>Help! I&#39;m in danger</Button>
+          <Button className='warning' onClick={(e) => this.handleClick(e, 'vulnerable')} block>I feel unsafe</Button>
         </div>
-        <SafeModal toggle={this.toggle} modal={this.state.modal} />
+        <SafeModal toggle={this.toggle} modal={this.state.modal} modalMessage={this.state.modalMessage} style={this.state.modalStyle} />
       </div>
     )
   }
--- src/components/SafeModal.js
+++ src/components/SafeModal.js
@@ -11,11 +11,11 @@ import './SafeModal.css'
 const SafeModal = (props) => {
   return (
     <Modal style={{opacity: 0.9}} isOpen={props.modal} toggle={props.toggle} autoFocus={false} backdrop='static'>
-      <ModalBody className='text' style={{backgroundColor: '#e60000'}}>
-        <h2>Danger alert sent!</h2>
+      <ModalBody className='text' style={props.style}>
+        <h2>{props.modalMessage}</h2>
         <p>We&#39;ve just sent a text message to your list</p>
       </ModalBody>
-      <ModalFooter className='footer' style={{backgroundColor: '#e60000'}}>
+      <ModalFooter className='footer' style={props.style}>
         <Button className='button' color='success' onClick={props.toggle} block>
           <h3>I feel safe now</h3>
         </Button>
