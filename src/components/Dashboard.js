3d181757434aa0af8dd422570094b106f0922ed4
Damen Hansen <damen.hansen@gmail.com>
2017-10-11 17:02:51 +1300

    remove commented code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -38,7 +38,6 @@ class Dashboard extends Component {
     }
     this.toggle = this.toggle.bind(this)
     this.handleClick = this.handleClick.bind(this)
-    // window.drift.api.widget.hide()
     window.drift.on('ready', function (api) {
   // hide the widget when it first loads
       api.widget.hide()
