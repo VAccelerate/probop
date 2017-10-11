82ab879601dccf01d29d7a78bfb820c49129ee4c
amandahogan <hogan.ans@gmail.com>
2017-10-11 15:03:28 +1300

    Tidy up helpline component

--- src/components/Helplines.js
+++ src/components/Helplines.js
@@ -1,95 +1,53 @@
 import React from 'react'
 import { ListGroup, ListGroupItem, ListGroupItemHeading } from 'reactstrap'
-import * as FontAwesome from 'react-icons/lib/fa'
 import './Helplines.css'
-import ImageHelpline from '../images/arrow1.png'
 
 const Helplines = (props) => {
   return (
     <div>
       <div className='ListGroup FontAwesome'>
         <ListGroup style={{width: '100vw'}}>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Alcohol & Drug Helpline
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800787797'>0800 787 797</a>
-            <span className='TopArrow'>
-              <a href='https://alcoholdrughelp.org.nz/'><img alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Alcohol & Drug Helpline</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800787797'>0800 787 797</a>
+            <a href='https://alcoholdrughelp.org.nz/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Are you Okay (Violence Helpline)
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800456450'>0800 456 450</a>
-            <span className='ImageHelpline'>
-              <a href='http://www.areyouok.org.nz/'><img className='ImageHelpline' alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Are you Okay (violence helpline)</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800456450'>0800 456 450</a>
+            <a href='http://www.areyouok.org.nz/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '210px'}}>
-            <ListGroupItemHeading>
-                Depression Hotline
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800456450'>0800 111 757</a> <br /><FontAwesome.FaCommenting size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='sms://4202'>4202</a>  &nbsp;&nbsp;&nbsp;
-            <span className='Arrow'>
-              <a href='https://depression.org.nz/'><img alt='' src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Depression Hotline</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800456450'>0800 111 757</a>
+            <img alt='' className='Sms' src='sms.png' /><a href='sms://4202'>4202</a>
+            <a href='https://depression.org.nz/'><img className='Arrow' alt='' src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Lifeline (Crisis Support)
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800543354'>0800 543 354</a>&nbsp;&nbsp;&nbsp;
-            <span className='CrisisArrow'>
-              <a href='https://www.lifeline.org.nz/'><img className='ImageHelpline' alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Lifeline (crisis support)</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800543354'>0800 543 354</a>
+            <a href='https://www.lifeline.org.nz/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Pacifika Helpline
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800787799'>0800 787 799</a> &nbsp;&nbsp;&nbsp;
-            <br /><FontAwesome.FaCommenting size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='sms://8681'>8681</a>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Pacifika Helpline</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800787799'>0800 787 799</a>
+            <img alt='' className='Sms' src='sms.png' /><a href='sms://8681'>8681</a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Outline (Sexuality & Gender Support)
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+648006885463'>0800 688 5463</a>&nbsp;&nbsp;&nbsp;
-            <span className='ImageHelpline'>
-              <a href='http://www.outline.org.nz/'><img className='ImageHelpline' alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Outline (sexuality & gender support)</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+648006885463'>0800 688 5463</a>
+            <a href='http://www.outline.org.nz/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '130px'}}>
-            <ListGroupItemHeading>
-                Shakti (Ethnic women support)
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800742584'>0800 742 584</a>&nbsp;&nbsp;&nbsp;
-            <span className='ShaktiArrow'>
-              <a href='http://shakti-international.org/'><img alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Shakti (ethnic women support)</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800742584'>0800 742 584</a>
+            <a href='http://shakti-international.org/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
-          <ListGroupItem style={{minHeight: '210px'}}>
-            <ListGroupItemHeading>
-                Youthline
-              </ListGroupItemHeading>
-            <FontAwesome.FaPhone size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='tel://+64800376633'>0800 376 633</a>&nbsp;&nbsp;&nbsp;
-            <br /><FontAwesome.FaCommenting size='10%' />&nbsp;&nbsp;&nbsp;
-            <a href='sms://234'>234</a>&nbsp;&nbsp;&nbsp;
-            <span className='Arrow'>
-              <a href='https://www.youthline.co.nz/'><img alt=''src={ImageHelpline} /></a>
-            </span>
+          <ListGroupItem>
+            <ListGroupItemHeading style={{fontSize: '5.0vw'}}>Youthline</ListGroupItemHeading>
+            <img alt='' className='Phone' src='phone.png' /><a href='tel://+64800376633'>0800 376 633</a>
+            <img alt='' className='Sms' src='sms.png' /><a href='sms://234'>234</a>
+            <a href='https://www.youthline.co.nz/'><img className='Arrow' alt=''src='arrow1.png' /></a>
           </ListGroupItem>
         </ListGroup>
       </div>
